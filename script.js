e41d303b926a81eb38bfa3713561e5dca4656d26
D-Pagey <dan.page91@gmail.com>
2017-10-03 00:16:46 +0100

    Display user's location on page load

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-var x = document.getElementById("location");
+/*
+- get latitude and longitude, store in variables
+- make an API call on London to test
+- input lat + long into API call to get weather
+- open weather API
+
+*/
+
+var latitude = document.getElementById("latitude");
+var longitude = document.getElementById("longitude");
 
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation isn't supported.";
-  }
-};
+    } else {
+      latitude.innerHTML = "Geolocation not supported";
+    }
+}
 
 function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+  latitude.innerHTML = " " + position.coords.latitude;
+  longitude.innerHTML = " " + position.coords.longitude;
 }
+
+getLocation();
