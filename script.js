ee2aa0362272e516539a7420fe622a802d3fc995
D-Pagey <dan.page91@gmail.com>
2017-10-08 00:20:04 +0100

    Weather dependent body class + relayout

--- script.js
+++ script.js
@@ -2,19 +2,25 @@
 + get latitude and longitude
 + make an API call on London to test
 + input lat + long into API call to get weather
-- display the data needed from JSON
++ display the data needed from JSON
 - toggle between Farenheit + Celcius
 - if statements to determine what to display. maybe have 3 images then a switch function?
 - investigate Fetch API
 (https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API)
 */
 
+/*
+Create "theme" array of different options - desciptions
+map that to a colour
+learn const vs var vs let
+*/
+
 
 // Get current location
-var latitude = document.getElementById("latitude");
-var longitude = document.getElementById("longitude");
+let latitude = document.getElementById("latitude");
+let longitude = document.getElementById("longitude");
 
-function getLocation() {
+const getLocation = () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
     } else {
@@ -22,29 +28,42 @@ function getLocation() {
     }
 }
 
-var update = document.getElementById("current");
-var place = document.getElementById("place");
+const update = document.getElementById("current");
+const place = document.getElementById("place");
+const temperature = document.getElementById("temperature");
+const max = document.getElementById("max");
+const min = document.getElementById("min");
 
 function showPosition(position) {
-  var lat = position.coords.latitude;
-  var lon = position.coords.longitude;
-  var finalURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=b8a569af62cc3d2b113f0b42813c6929&units=metric";
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const finalURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=b8a569af62cc3d2b113f0b42813c6929&units=metric";
   latitude.innerHTML = " " + lat;
   longitude.innerHTML = " " + lon;
 
-  var request = new XMLHttpRequest();
+  const request = new XMLHttpRequest();
   request.open('GET', finalURL);
   request.responseType = 'json';
   request.send();
   request.onload = function() {
-  var weatherData = request.response;
+  const weatherData = request.response;
   console.log(weatherData);
   update.innerHTML = " " + weatherData.weather[0].description;
   place.innerHTML = "" + weatherData.name;
+  temperature.innerHTML = "" + weatherData.main.temp;
+  max.innerHTML = "" + weatherData.main.temp_max;
+  min.innerHTML = "" + weatherData.main.temp_min;
+    const description = weatherData.weather[0].description;
+    const themeClass = theme[description];
+document.body.className = themeClass || 'default';
 }
 
 }
-
+const theme = {
+  'clear sky': 'grey',
+  'udefined': 'black',
+  'broken clouds': 'grey',
+  'shower rain': 'grey',
+  'overcast clouds': 'black'
+}
 getLocation();
-
-// Make the API call
