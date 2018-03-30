2379c82be56d0d3b100a2f92bf20c345b4ef1a8e
D-Pagey <dan.page91@gmail.com>
2018-03-30 01:06:01 +0100

    Converting to stateless functional components.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 
 import './Footer.css';
 
-class Footer extends Component {
-  render() {
-    return (
-      <footer className="footer">
-        <p>Written and coded by <a href="https://www.freecodecamp.org/d-pagey"
-          target="_blank" rel="noopener noreferrer" className="freecodecamp">
-          Pagey</a></p>
-      </footer>
-    )
-  }
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Written and coded by <a href="https://www.freecodecamp.org/d-pagey"
+        target="_blank" rel="noopener noreferrer" className="freecodecamp">
+        Pagey</a></p>
+    </footer>
+  )
 }
 
 export default Footer;
--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -2,12 +2,10 @@ import React, { Component } from 'react';
 
 import './Image.css';
 
-class Image extends Component {
-  render() {
-    return (
-      <div className="img-container"></div>
-    )
-  }
+function Image() {
+  return (
+    <div className="img-container"></div>
+  )
 }
 
 export default Image;
