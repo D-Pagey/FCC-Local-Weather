e3b629b311929e264fdaa120ab7a64647313d0c5
D-Pagey <dan.page91@gmail.com>
2018-04-01 01:10:02 +0100

    Bug: can't get the geolocation to work in CRA.

--- README.md
+++ README.md
@@ -6,13 +6,9 @@
   desert) depending on the weather.
 - [x] I can push a button to toggle between Fahrenheit and Celsius.
 
-I learnt a lot about working with APIs in native JavaScript on this project. At
-first I called the API using an XML HTTP Request and then I used the Fetch API.
-I also created separate CSS classes for each type of weather.
-
-First time round I styled the project using Bootstrap, however I have since
-rewritten the CSS using FlexBox and redesigned it mobile-first. I've also
-replaced the CSS with SCSS for the media queries.
+The most recent version of this project was written in React using
+Create-React-App. It was really interesting to see how to manage the various
+states of components in React versus how I handled it in the native version. 
 
 **Improvements:**
 - I've created a helper function but can't work out how to implement it. In the
--- src/utilities/Location.js
+++ src/utilities/Location.js
@@ -1,4 +1,4 @@
-export function getWeather() {
+export default function getUrl() {
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
@@ -10,14 +10,13 @@ export function getWeather() {
     console.log(position);
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const finalUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=b8a569af62cc3d2b113f0b42813c6929&units=metric";
-
-    fetch(finalUrl)
-      .then((resp) => resp.json())
-      .then(function(data) {
-        console.log(data)
-        return data;
-      });
+    let finalUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    lat + "&lon=" + lon + "&appid=b8a569af62cc3d2b113f0b42813c6929&units=metric";
+    return finalUrl;
     }
 
 }
+
+// I need to put together a URL to fetch from. I then need to use that in my
+// App.js file. I can't get the getCurrentPosition to consistently work here.
+// However, in my native app it works fine. 
